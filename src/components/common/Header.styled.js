ed7b2716e2dbfc7ce9e049b5a77f254f29b84839
ss0yaa <syp0205@sookmyung.ac.kr>
2025-11-02 16:00:11 +0900

    Feat/#4/Header 공통 컴포넌트 생성

---
+++ src/components/common/Header.styled.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components'
+
+export const HeaderBar = styled.header`
+  display: grid;
+  grid-template-columns: 24px 1fr 24px;
+  align-items: center;
+  padding: 18px 16px;
+  border-bottom: 0.5px solid var(--Neutral-100, #e7e7e7);
+`
+
+export const Icon = styled.img``
+
+export const Title = styled.p`
+  font-weight: 500;
+  font-size: 20px;
+  line-height: 28px;
+  text-align: center;
+  color: #333333;
+`
+
+export const Spacer = styled.div`
+  width: 24px;
+  height: 24px;
+`
