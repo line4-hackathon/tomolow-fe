bd19fb6ab3cad276bdb2888c76f89f05dcda41d0
ss0yaa <syp0205@sookmyung.ac.kr>
2025-11-03 01:34:35 +0900

    Feat/#6/회원가입 페이지 생성

--- src/router.jsx
+++ src/router.jsx
@@ -1,12 +1,24 @@
 import { createBrowserRouter } from 'react-router-dom'
 import App from './App'
 import LoginPage from './pages/signup/LoginPage'
+import SignupPage1 from './pages/signup/SignupPage1'
+import SignupPage2 from './pages/signup/SignupPage2'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
-    children: [{ path: 'login', element: <LoginPage /> }],
+    children: [
+      { path: 'login', element: <LoginPage /> },
+      {
+        path: 'signup/1',
+        element: <SignupPage1 />,
+      },
+      {
+        path: 'signup/2',
+        element: <SignupPage2 />,
+      },
+    ],
   },
 ])
 
