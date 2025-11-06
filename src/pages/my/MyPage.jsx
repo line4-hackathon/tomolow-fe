42957933418a9b0a23c9030eec91f6e058894c92
ss0yaa <syp0205@sookmyung.ac.kr>
2025-11-06 13:34:22 +0900

    Feat/#17/메인 마이페이지 제작

--- src/pages/my/MyPage.jsx
+++ src/pages/my/MyPage.jsx
@@ -1,11 +1,33 @@
 import React, { useState } from 'react'
-import styled from 'styled-components'
+import * as S from './MyPage.styled'
 import Header from '@/components/common/Header'
+import ButtonItem from '@/components/my/ButtonItem'
+import savedIcon from '@/assets/icons/icon-saved-chatting.svg'
+import moneyIcon from '@/assets/icons/icon-money-recharge.svg'
+import editIcon from '@/assets/icons/icon-edit-info.svg'
+import qnaIcon from '@/assets/icons/icon-qna.svg'
 
 const MyPage = () => {
   return (
     <>
       <Header title='MY' />
+      <S.Container>
+        <S.NameContainer>
+          <S.Name>{`멋쟁이사자`} </S.Name>
+          <S.Span> 님</S.Span>
+        </S.NameContainer>
+        <S.MoneyContainer>
+          <S.Label>보유 중인 머니</S.Label>
+          <S.Money>{(160434464).toLocaleString()}원</S.Money>
+        </S.MoneyContainer>
+        <S.ButtonContainer>
+          <ButtonItem icon={savedIcon} label={`저장된 채팅`} />
+          <ButtonItem icon={moneyIcon} label={`머니 충전`} />
+          <ButtonItem icon={editIcon} label={`정보 수정`} />
+          <ButtonItem icon={qnaIcon} label={`문의하기`} />
+        </S.ButtonContainer>
+        <S.Logout>로그아웃</S.Logout>
+      </S.Container>
     </>
   )
 }
---
+++ src/pages/my/MyPage.styled.js
@@ -0,0 +1,71 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  padding: 32px 16px;
+  background: var(--Neutral-50, #f6f6f6);
+`
+
+export const NameContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: end;
+  gap: 4px;
+  padding-bottom: 32px;
+`
+
+export const Name = styled.p`
+  color: var(--Neutral-900, #333);
+  font-size: 20px;
+  font-weight: 500;
+  line-height: 28px;
+`
+
+export const Span = styled.span`
+  color: var(--Neutral-900, #333);
+  font-size: 16px;
+  font-weight: 500;
+  line-height: 24px;
+`
+export const MoneyContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+  border-radius: var(--Spacing-L, 16px);
+  background: var(--Primary-200, #ccdceb);
+  box-shadow: 0 4px 12px 0 rgba(0, 0, 0, 0.08);
+  padding: 16px;
+`
+export const Label = styled.p`
+  color: var(--Neutral-900, #333);
+  font-size: 20px;
+  font-weight: 400;
+  line-height: 28px;
+`
+
+export const Money = styled.p`
+  color: var(--Neutral-900, #333);
+  font-size: 20px;
+  font-weight: 500;
+  line-height: 28px;
+`
+export const ButtonContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 24px 0;
+  padding: 24px 16px;
+  gap: 32px;
+  border-radius: var(--Radius-M, 12px);
+  background: var(--Neutral-0, #fff);
+  box-shadow: 0 4px 12px 0 rgba(0, 0, 0, 0.08);
+`
+export const Logout = styled.p`
+  color: var(--Neutral-900, #333);
+  font-size: 12px;
+  font-weight: 400;
+  line-height: 16px;
+  cursor: pointer;
+`
