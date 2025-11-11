f7f4659764230d6eb30e4d2ac9aa0990fd138b6a
ss0yaa <syp0205@sookmyung.ac.kr>
2025-11-12 02:15:49 +0900

    Api/#58/머니 충전 연동

--- src/components/my/AdList.jsx
+++ src/components/my/AdList.jsx
@@ -1,20 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
 import styled from 'styled-components'
 import ImgAd from '@/assets/images/img-ad-list.svg?react'
 import RightArrow from '@/assets/icons/icon-right-arrow.svg?react'
+import Toast from '../common/Toast'
+import Loading from '../common/Loading'
 
-const AdList = ({ onClick }) => {
+const AdList = () => {
+  const [loading, setLoading] = useState(false)
+  const [toastMsg, setToastMsg] = useState('')
+  const apiUrl = import.meta.env.VITE_API_BASE_URL
+  const handleAdClick = async () => {
+    try {
+      setLoading(true)
+      const token = localStorage.getItem('accessToken')
+      const res = await axios.post(
+        `${apiUrl}/api/mypage/cash/ad`,
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        },
+      )
+      setToastMsg(`광고 보상이 지급 완료되었어요.`)
+    } catch (err) {
+      console.log(err)
+    } finally {
+      setLoading(false)
+    }
+  }
   return (
-    <List onClick={onClick}>
-      <Left>
-        <ImgAd />
-        <Column>
-          <Title>광고 시청하기</Title>
-          <Subtitle>회당 {(500000).toLocaleString()}원</Subtitle>
-        </Column>
-      </Left>
-      <RightArrow />
-    </List>
+    <>
+      <List onClick={handleAdClick}>
+        <Left>
+          <ImgAd />
+          <Column>
+            <Title>광고 시청하기</Title>
+            <Subtitle>회당 {(500000).toLocaleString()}원</Subtitle>
+          </Column>
+        </Left>
+        <RightArrow />
+      </List>
+      {toastMsg && <Toast msg={toastMsg} onClose={() => setToastMsg('')} />}
+    </>
   )
 }
 
