0f0280f1e8808ddba0565a3f1739109f3cc88657
ss0yaa <syp0205@sookmyung.ac.kr>
2025-11-12 16:30:28 +0900

    Refactor/#62/페이지 이동을 URL 변경으로 가능하게 재수정

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,11 @@ import { Outlet, Navigate, useLocation } from 'react-router-dom'
 import { TypeProvider } from './contexts/TypeContext.jsx'
 import { AuthProvider, useAuth } from './contexts/AuthContext'
 
-function ProtectedPath() {
-  const { isLoggedIn } = useAuth()
-  const location = useLocation()
-
-  if (!isLoggedIn && !['/login', '/signup/1', '/signup/2'].includes(location.pathname)) {
-    return <Navigate to='/login' replace />
-  }
-
-  return <Outlet />
-}
 function App() {
   return (
     <AuthProvider>
       <TypeProvider>
-        <ProtectedPath />
+        <Outlet />
       </TypeProvider>
     </AuthProvider>
   )
