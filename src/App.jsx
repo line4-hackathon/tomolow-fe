4b5f5d778cde62d4139a2c1eadca2591666a72c9
Jeong mokjin <jungmokjin@gmail.com>
2025-11-14 01:36:36 +0900

    Design/#52/버튼 모션 적용

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import './App.css'
 import { Outlet, Navigate, useLocation } from 'react-router-dom'
 import { TypeProvider } from './contexts/TypeContext.jsx'
 import { AuthProvider, useAuth } from './contexts/AuthContext'
+import { GlobalButtonStyles } from './components/common/GlobalButtonMotion'
 
 function App() {
   return (
     <AuthProvider>
       <TypeProvider>
+        <GlobalButtonStyles/>
         <Outlet />
       </TypeProvider>
     </AuthProvider>
