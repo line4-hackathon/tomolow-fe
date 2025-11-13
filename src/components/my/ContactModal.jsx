553cbec72dcf24f8df4c40c020174b72d40ded51
ss0yaa <syp0205@sookmyung.ac.kr>
2025-11-14 01:57:19 +0900

    Design/#83/버튼 효과 적용

--- src/components/my/ContactModal.jsx
+++ src/components/my/ContactModal.jsx
@@ -27,4 +27,32 @@ const Button = styled.button`
   border: none;
   background: var(--Primary-500, #4880af);
   cursor: pointer;
+
+  transition:
+    background-color 0.1s ease,
+    transform 0.1s ease,
+    font-size 0.1s ease;
+
+  &::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0);
+    pointer-events: none;
+    border-radius: 12px;
+    transition: background-color 0.1s ease;
+  }
+
+  &:active {
+    font-size: 0.95em;
+
+    transform: scale(0.98);
+
+    &::before {
+      background-color: rgba(0, 0, 0, 0.2);
+    }
+  }
 `
